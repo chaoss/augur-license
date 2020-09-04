082100c2eb1a05aeea76b0cfd4982448ad303bdb
Sean P. Goggins <outdoors@acm.org>
2020-09-04 14:52:53 +0200

    trying to make augur-spdx work easy

--- scripts/install/install-spdx-sudo.sh
+++ scripts/install/install-spdx-sudo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get install git python3-pip postgresql && sudo apt-get install libpq-dev
 sudo apt-get install libglib2.0-dev libjsoncpp-dev libjson-c-dev
-pip3 install .
+pip3 install -r requirements.txt 

1ab8d4431cee5cf01628f09f6312854dd5cdb9e1
Sean P. Goggins <outdoors@acm.org>
2020-09-07 03:46:56 +0200

    Fix some preference things

--- setup.py
+++ setup.py
@@ -1,42 +1,51 @@
-import io
-import os
-import re
-
-from setuptools import find_packages
 from setuptools import setup
 
-def read(filename):
-    filename = os.path.join(os.path.dirname(__file__), filename)
-    text_type = type(u"")
-    with io.open(filename, mode="r", encoding='utf-8') as fd:
-        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+_dosocs2_version = '0.16.1'
+
+install_requires=[
+    'jinja2',
+    'python-magic',
+    'docopt',
+    'SQLAlchemy',
+    'psycopg2'
+    ]
+
+tests_require=[
+    'pytest',
+    'mock'
+    ]
 
 setup(
-    name="template_worker",
-    version="0.0.0",
-    url="https://github.com/chaoss/augur",
-    license='MIT',
-    author="Augur Team",
-    author_email="gabe@gabehe.im",
-    description="Template worker to be used as an example",
-    packages=find_packages(exclude=('tests',)),
-    install_requires=[
-        'flask',
-        'requests',
-        'psycopg2-binary',
-        'click',
-        'pathlib'
-    ],
-    entry_points={
-        'console_scripts': [
-            'template_worker_start=template_worker.runtime:main',
-        ],
-    },
+    name='dosocs2',
+    version=_dosocs2_version,
+    description='SPDX 2.0 document creation and storage',
+    long_description='',
+    url='https://github.com/DoSOCSv2/DoSOCSv2',
+    author='Thomas T. Gurney',
+    author_email='tgurney@unomaha.edu',
+    license='GNU General Public License v2 or later (GPLv2+)',
+
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-    ]
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Python :: 2.7',
+        'Environment :: Console',
+        ],
+
+    keywords='spdx license licenses',
+    packages=['dosocs2', 'dosocs2.scanners'],
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={
+        'tests': install_requires + tests_require,
+        },
+    package_data={'dosocs2': ['templates/*', 'fixtures/*']},
+
+    entry_points={'console_scripts': ['dosocs2=dosocs2.dosocs2:main']},
+    test_suite='py.test',
+
+    zip_safe=False,
 )
